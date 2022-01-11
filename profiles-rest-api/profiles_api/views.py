c5e17a6c2cd0200b89f48443cf98dfc3a524fffb
paulo <pauloadmendonca@gmail.com>
2022-01-11 00:01:04 -0300

    serializers

--- profiles-rest-api/profiles_api/views.py
+++ profiles-rest-api/profiles_api/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from profiles_api import serializers
+
 
 # Create your views here.
+
+class HelloApiView (APIView):
+
+    serializer_class = serializers.HelloSerializer
+
+    def get(self, request, format=None):
+        an_apiview = [
+            'Uses HTTP methods as functions (get, post, patch, put, delete)',
+            'Is similar to a traditional Django View',
+            'Gives you the most control over your logic',
+            'Is mapped manually to URLs',
+        ]
+
+        return Response({'message': 'Hello!', 'an_apiview': an_apiview})
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}, seu hijo de puta'
+            return Response({'message': message})
+
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+    def put(self, request, pk=None):
+        "replace de um novo objeto"
+
+        return Response({'method': 'PUT'})
+
+    def patch(self, request, pk=None):
+        "metelança de um novo objeto"
+
+        return Response({'method': 'PATCH'})
+
+    def delete(self, request, pk=None):
+        "deletança de um novo objeto"
+
+        return Response({'method': 'DELETE'})
